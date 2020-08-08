ee6df5e920163558144e9ab5575d670f8859bfc5
Clare Jung <clare.jung@hotmail.com>
2020-08-08 18:00:33 +0900

    resulresult page

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,19 +1,20 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 
 const QuestionBox = ({ question, options, selected }) => {
     const [answer, setAnswer] = useState(options);
-    return(
+    return (
         <div className="questionBox">
             <div className="question">{question}</div>
             {answer.map((text,index) => (
                 <button key={index} className="answerBtn" onClick={() => {
                     setAnswer([text]);
+                    selected(text);
                 }}>
                     {text}
                 </button>
             ))}
         </div>
     )
-}
+};
 
 export default QuestionBox;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./assets/style.css";
 import quizService from "./quizService";
 import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
 
 class QuizBee extends Component {
     state = {
@@ -27,13 +28,20 @@ class QuizBee extends Component {
             responses: this.state.responses < 5 ? this.state.responses + 1 : 5
         });
     }
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0
+        })
+    }
     componentDidMount() {
         this.getQuestions();
     }
     render() {
         return (
             <div className="container">
-                <div className="title">QuizBee</div>
+                <div className="title">Trivia Quiz</div>
                 {this.state.questionBank.length > 0 && 
                 this.state.responses < 5 &&
                 this.state.questionBank.map(({question, answers, 
@@ -46,7 +54,8 @@ class QuizBee extends Component {
                         />
                     )
                     )}
-                    {this.state.responses === 5 ? <h2>{this.state.score}</h2> : null}
+                    {this.state.responses === 5 ? (
+                    <Result score={this.state.score} playAgain={this.playAgain} />) : null}
             </div>
         );
     }
