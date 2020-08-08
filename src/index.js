5715b831daae61b75d36ee5acdfb922ab2ed7201
Clare Jung <clare.jung@hotmail.com>
2020-08-08 17:40:07 +0900

    results disdisplay

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
 import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
 
 class QuizBee extends Component {
     state = {
-        questionBank: []
+        questionBank: [],
+        score: 0,
+        responses: 0
     };
     getQuestions = () => {
         quizService().then(question => {
@@ -14,6 +17,16 @@ class QuizBee extends Component {
             })
         })
     }
+    computeAnswer = (answer,correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        });
+    }
     componentDidMount() {
         this.getQuestions();
     }
@@ -21,8 +34,19 @@ class QuizBee extends Component {
         return (
             <div className="container">
                 <div className="title">QuizBee</div>
-                {this.state.questionBank.length > 0 && this.state.questionBank.map(({question, answers, 
-                    correct, questionId}) => (<h4>{question}</h4>))}
+                {this.state.questionBank.length > 0 && 
+                this.state.responses < 5 &&
+                this.state.questionBank.map(({question, answers, 
+                    correct, questionId}) => (
+                        <QuestionBox 
+                        question={question}
+                        options={answers}
+                        key={questionId}
+                        selected={answer => this.computeAnswer(answer,correct)}
+                        />
+                    )
+                    )}
+                    {this.state.responses === 5 ? <h2>{this.state.score}</h2> : null}
             </div>
         );
     }
