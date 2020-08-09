c862ad7ca28f6b45208fa627b4ada3bc41444fee
Clare Jung <clare.jung@hotmail.com>
2020-08-09 23:36:21 +0900

    Updates

--- precache-manifest.5ac24ac63489ea6d6afd2bd0c5963b3b.js
+++ precache-manifest.5ac24ac63489ea6d6afd2bd0c5963b3b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c33758a2ec4da18164934f6195730cc",
+    "revision": "df93f40a85bcd7f1f09c62430d650968",
     "url": "/quizbee/index.html"
   },
   {
-    "revision": "1d9b57bb4a519e9201b4",
-    "url": "/quizbee/static/css/main.0f93a89c.chunk.css"
+    "revision": "6d34b05f6308bf02f6e4",
+    "url": "/quizbee/static/css/main.bc7ae181.chunk.css"
   },
   {
     "revision": "431e44eca0f0169fbbc7",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/quizbee/static/js/2.9f25e938.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1d9b57bb4a519e9201b4",
-    "url": "/quizbee/static/js/main.9928ec74.chunk.js"
+    "revision": "6d34b05f6308bf02f6e4",
+    "url": "/quizbee/static/js/main.68f06662.chunk.js"
   },
   {
     "revision": "1192c94079eb9535dd92",
